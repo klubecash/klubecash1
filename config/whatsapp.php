db2758df24d31b6b2ad38922ab0ef1e9602f5f67
kauamatheus-ksml <148459355+kauamatheus-ksml@users.noreply.github.com>
2025-10-02 22:29:50 -0300

    WHATSAPP_ENABLED

--- config/whatsapp.php
+++ config/whatsapp.php
@@ -9,8 +9,8 @@ if ($whatsappEnabled === false) {
     $whatsappEnabled = true; // habilitado por padr�o quando o arquivo � inclu�do
 }
 
-define('WHATSAPP_ENABLED', filter_var($whatsappEnabled, FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE) ?? (bool)$whatsappEnabled);
-define('WHATSAPP_BASE_URL', rtrim(getenv('WHATSAPP_BASE_URL') ?: 'http://148.230.73.190:21465', '/'));
+define('WHATSAPP_ENABLED', true);
+define('WHATSAPP_BASE_URL', rtrim(getenv('WHATSAPP_BASE_URL') ?: 'http://klubezap.klubecash.com:8080', '/'));
 define('WHATSAPP_SESSION_NAME', getenv('WHATSAPP_SESSION') ?: 'mySession');
 define('WHATSAPP_API_TOKEN', getenv('WHATSAPP_TOKEN') ?: '$2b$10$irMbsdbXWXCSvjYgD3hC4.y5Gy83dTYoi6vMem948NUjxn5Neoj_G');
 define('WHATSAPP_HTTP_TIMEOUT', (int)(getenv('WHATSAPP_HTTP_TIMEOUT') ?: 20));
@@ -23,4 +23,7 @@ define('WHATSAPP_TEMPLATE_LANGUAGE', getenv('WHATSAPP_TEMPLATE_LANGUAGE') ?: 'pt
 
 define('WHATSAPP_DEFAULT_FALLBACK_MESSAGE', 'N�o foi poss�vel completar o envio pelo WhatsApp. Tente novamente mais tarde.');
 ?>
+
+
+
 
